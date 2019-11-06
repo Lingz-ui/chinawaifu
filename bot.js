b166763cd9d23c0667bc3727af769066395771b0
Hestia <56937827+hestia2412@users.noreply.github.com>
2019-11-06 12:17:38 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ module.exports = bot;
 require('./events/onMessage');
 require('./events/onError');
 require('./helpers/loadcommands').load();
-bot.login("NTYyMTAyODE2NDQ2MDg3MTc5.XcH6Fg.MOk0RSI8LhBeeFwT4KMNtiQGxxo");
+bot.login("");
 
 bot.conf = {
 	prefix: '^',
