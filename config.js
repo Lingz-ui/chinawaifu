54a42633c6028cc9c568fe907187a9657b1f1aff
yumi <caubedondoc.ls01@gmail.com>
2019-11-09 21:07:33 +0700

    xamxi

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 	botToken: "NjQxNzgwOTIyNDQ1ODU2NzY4.XcP1mw.I1YPfm_hu_ZBpKl8FbIoWKInpW4"
 }
@@ -6,3 +7,8 @@ module.exports = {
 	botToken: "put your bot token here"
 }
 >>>>>>> 6105eb50948f2e6222c012002d1197331984441b
+=======
+	//botToken: "NjQxNzgwOTIyNDQ1ODU2NzY4.XcP1mw.I1YPfm_hu_ZBpKl8FbIoWKInpW4"
+	botToken:"NTIwNTY2MjY1ODI4NDc0ODgx.XcY8eA.P5fKVXjdP7HAOAzOkliayuLhDgU"
+}
+>>>>>>> Stashed changes
